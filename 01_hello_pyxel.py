078e5394e2cfdb32f12f3cef530517e7dadd71dd
Julio Sosa <bla@abz.com>
2022-12-05 01:01:19 +0100

    remove OOD

--- 01_hello_pyxel.py
+++ 01_hello_pyxel.py
@@ -1,20 +1,17 @@
 import pyxel
 
 
-class App:
-    def __init__(self):
-        pyxel.init(160, 120, title="Hello Pyxel")
-        pyxel.image(0).load(0, 0, "assets/pyxel_logo_38x16.png")
-        pyxel.run(self.update, self.draw)
-
-    def update(self):
-        if pyxel.btnp(pyxel.KEY_Q):
-            pyxel.quit()
-
-    def draw(self):
-        pyxel.cls(0)
-        pyxel.text(55, 41, "Hello, Pyxel!", pyxel.frame_count % 16)
-        pyxel.blt(61, 66, 0, 0, 0, 38, 16)
+def UpdateKeys():
+    if pyxel.btnp(pyxel.KEY_Q):
+        pyxel.quit()
 
 
-App()
+def DrawStuff():
+    pyxel.cls(col=0)
+    pyxel.text(x=55, y=41, s="Hello Pyxel!", col=pyxel.frame_count % 16)
+    pyxel.blt(img=0, x=61, y=66, w=16, h=16, u=0, v=0)
+
+
+pyxel.init(width=160, height=120, title="Hello Pyxel")
+pyxel.image(0).load(0, 0, 'assets/pyxel_logo_38x16.png')
+pyxel.run(UpdateKeys, DrawStuff)
